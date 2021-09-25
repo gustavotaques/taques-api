83b30b1c560277da56914e236ad8168ab0e23689
gustavotaques <gustavotaques@users.noreply.github.com>
2021-09-25 16:50:23 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="gustavotaques"
+original_name="taques_api"
+original_urlname="taques-api"
+original_description="Awesome taques_api created by gustavotaques"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv taques_api $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- taques_api/app.py
+++ taques_api/app.py
@@ -1,5 +1,5 @@
 """
-project_name base module.
+taques_api base module.
 """
 from fastapi import FastAPI
 
